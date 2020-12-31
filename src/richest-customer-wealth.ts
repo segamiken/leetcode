81c7dc8e313025d232b09999da254223e96b3340
KEN <ken.s.vollyball@gmail.com>
2020-12-31 17:44:04 +0900

    Solve richest-customer-wealth

---
+++ src/richest-customer-wealth.ts
@@ -0,0 +1,14 @@
+function maximumWealth(accounts: number[][]): number {
+    let customers_wealths = [];
+    for (let i = 0; i < accounts.length; i ++ ) {
+        let customer_wealth = 0;
+        for (let m = 0; m < accounts[i].length; m ++) {
+            customer_wealth = customer_wealth + accounts[i][m];
+        }
+        customers_wealths.push(customer_wealth);
+    }
+    const max_wealth = customers_wealths.reduce(function(a, b) {
+        return Math.max(a, b);
+    });
+    return max_wealth;
+};
