60a4b0d93f7578a4cd3ad2b8c6c6d84e98d8c5a3
KEN <ken.s.vollyball@gmail.com>
2021-01-02 20:25:50 +0900

    Solve add strings without using any built-in BigInteger library or converting the inputs to integer directly.

---
+++ src/add-strings.ts
@@ -0,0 +1,25 @@
+function addStrings(num1: string, num2: string): string {
+    let res: number[] = [];
+    let carry: number = 0;
+    
+    let p1: number = num1.length -　1;
+    let p2: number = num2.length - 1;
+    
+    while (p1 >= 0 || p2 >= 0) {
+        let x1 = p1 >= 0 ? Number(num1.charAt(p1)) : 0;
+        let x2 = p2 >= 0 ? Number(num2.charAt(p2)) : 0;
+        let value = (x1 + x2 + carry) % 10;
+        carry = (x1 + x2 + carry) / 10　>= 1 ?  Math.floor((x1 + x2 + carry) / 10) : 0;
+        res.unshift(value);
+        p1 --;
+        p2 --;
+    }
+    
+    if (carry > 0) {
+        res.unshift(carry);
+    }
+    
+    return res.join("");
+};
+
+console.log(addStrings("9", "9"));
